68cd12ba599de7169570d84da5b4159e1704fb67
HugoMorales <hugo.156@hotmail.com>
2023-03-21 11:17:43 -0600

    Practica con Rafael Encinas

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-// Escribe tu código aquí.
+const miArreglo = ["cadena1", "cadena2", "cadena3"];
 
+function mostrarListaEnDOM(arreglo) {
+    // Obtiene el elemento del DOM donde se agregará la lista
+    const lista = document.getElementById('miLista');
+  
+    // Crea un arreglo vacío para almacenar los elementos HTML de la lista
+    const elementosLista = [];
+    
+    // Recorre el arreglo y crea un elemento HTML para cada cadena
+    for (let i = 0; i < arreglo.length; i++) {
+      const elemento = document.createElement('li'); // Crea un nuevo elemento <li>
+      elemento.textContent = arreglo[i]; // Establece el texto del elemento
+      elementosLista.push(elemento); // Agrega el elemento al arreglo de elementos de lista
+    }
+    
+    // Agrega cada elemento de la lista al elemento del DOM
+    for (let i = 0; i < elementosLista.length; i++) {
+      lista.appendChild(elementosLista[i]); // Agrega el elemento al final de la lista en el DOM
+    }
+  }
 
-const arreglo = ["test", "test2"];
-
-const palabra = "test";
-
-
-function bigWord () 
+  mostrarListaEnDOM(miArreglo);
